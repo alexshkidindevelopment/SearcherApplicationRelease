621c6be6d6859875bc65ba88adaab41051df7322
WIN-JOCTCGTF6GD\User <shkid111@gmail.com>
2018-07-25 00:21:57 +0300

    change search engine settings model

--- SearcherApplication.Models/DataModels/SearchEngineSettings.cs
+++ SearcherApplication.Models/DataModels/SearchEngineSettings.cs
@@ -4,15 +4,10 @@ namespace SearcherApplication.Models.DataModels
 {
     public class SearchEngineSettings
     {
-        [Required]
-        [MaxLength(50), MinLength(4)]
         public string Name { get; set; }
-
-        [Required]
-        [MaxLength(250), MinLength(4)]
+        
         public string ApiKey { get; set; }
-
-        [MaxLength(250), MinLength(4)]
+        
         public string SearchEngineId { get; set; }
     }
 }
